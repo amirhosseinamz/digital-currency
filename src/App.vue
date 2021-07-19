0188861ece66ab602430506884ec4c734d51fd12
amirhosseinamz <amirhoseinamz45@gmail.com>
2021-07-19 18:13:53 +0430

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
@@ -11,20 +7,16 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
+  body {
+    margin: 0;
+    padding: 0;
+  }
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
   }
 }
 </style>
